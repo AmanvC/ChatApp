9d2d81dac4011158f06597697d51c2b759590c1b
Aman <amanvarshney.varshney@gmail.com>
2023-05-20 04:51:33 +0530

    Added Routing and ChakraUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,51 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import ChatPage from "./pages/chatPage/ChatPage";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Loader from "./components/loader/Loader";
 
 function App() {
+  const { currentUser, loading } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />;
+    }
+    return children;
+  };
+
+  const LoggedInProtectionRoute = ({ children }) => {
+    if (currentUser) {
+      return <Navigate to="/chats" />;
+    }
+    return children;
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/chats" element={<ChatPage />} />
+        <Route
+          path="/"
+          element={
+            <LoggedInProtectionRoute>
+              <Homepage />
+            </LoggedInProtectionRoute>
+          }
+        />
+        <Route
+          path="/chats"
+          element={
+            <ProtectedRoute>
+              <ChatPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );

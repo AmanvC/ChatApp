9137a95c0309df464ffb8879431521e4ccc46cf1
Aman <amanvarshney.varshney@gmail.com>
2023-05-26 19:09:34 +0530

    Created API to create a message, and get all messages for a particular chat

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use("/auth", require("./auth"));
 router.use("/users", require("./users"));
 router.use("/chats", require("./chats"));
+router.use("/messages", require("./messages"));
 
 module.exports = router;
